5719944694d8d6d9dc05dadabd11942e68375e5f
YeHtutAung <yehaung.nov@gmail.com>
2023-06-29 17:06:46 +0200

    input more static players in Game

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['John', 'Marco', 'Erika'];
     public stack: string[] = [];
     public playedCard: string[] = [];
     public currentPlayer: number = 0;
